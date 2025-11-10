a89a364d975c51ba114a0d17d9b80b696eb38115
Zxi01 <cameron.m.jennings@hotmail.com>
2025-11-10 16:02:31 +0000

    prepartion for heroku deployment

---
+++ hello_world/views.py
@@ -0,0 +1,12 @@
+from urllib import request
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+       if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+       else:
+        return HttpResponse(request.method)
